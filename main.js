18a984f4a43fe4531045c09395fad740444b62ac
Danielli7 <148447477+Danielli7@users.noreply.github.com>
2024-03-22 11:22:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const botoes = querySelectorAll(".botao");
-for(let i= 0; i< botoes.lenght; i++){
+const botoes = document.querySelectorAll(".botao");
+for(let i= 0; i< botoes.length; i++){
     botoes[i].onclick = function(){
-        for(j=0; j<botoes.lenght; j++){
-            botoes[j].claslist.remove("ativo");
+        for (let j=0; j<botoes.length; j++){
+            botoes[j].classList.remove("ativo");
         }
         botoes[i].classlist.add("ativo");
     }
